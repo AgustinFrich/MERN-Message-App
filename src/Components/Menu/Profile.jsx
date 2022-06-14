852b4d3e06e4d662f085b638aa7e8022dd488fc4
AgustinFrich <agustinfrich@gmail.com>
2022-06-14 16:40:58 -0300

    Working app

--- src/Components/Menu/Profile.jsx
+++ src/Components/Menu/Profile.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useSocket } from "../../Providers/socketProvider";
 const Profile = ({ user }) => {
-  const { getUsers, socketUsers, detectOtherUser, otherUser } = useSocket();
+  const { getUsers, socketUsers, detectOtherUser } = useSocket();
   const [get, setGet] = useState(true);
   useEffect(() => {
     if (get) {
