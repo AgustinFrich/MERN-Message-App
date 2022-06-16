4d975487b5e96dfcfdc6c501f9006e9581dae988
AgustinFrich <agustinfrich@gmail.com>
2022-06-16 10:28:40 -0300

    Centered acces form

--- src/Components/Menu/Main.jsx
+++ src/Components/Menu/Main.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
-  return (
-    <div>
-      <h1>Main</h1>
-      <a href="/signup">Get started</a>
-    </div>
-  );
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/signup");
+  }, []);
+
+  return <></>;
 };
 
 export default Main;
--- src/Components/Menu/SignUp/AccesForm.jsx
+++ src/Components/Menu/SignUp/AccesForm.jsx
@@ -30,66 +30,68 @@ const AccesForm = ({
     );
   }
   return (
-    <div className="SignUp">
-      <CardGroup className="SignUp-FirstCard">
-        <Card className="SignUp-FirstCard">
-          <Card.Img src={image} alt="Card image" />
-          <Card.ImgOverlay className="SignUp-image">
-            <Card.Title as={"h1"} className="SignUp-imageTitle">
-              {type}
-            </Card.Title>
-            <Card.Text>{message}</Card.Text>
-            <hr />
-          </Card.ImgOverlay>
-        </Card>
+    <div className="d-flex justify-content-center border-0">
+      <div className="SignUp">
+        <CardGroup className="SignUp-FirstCard">
+          <Card className="SignUp-FirstCard">
+            <Card.Img src={image} alt="Card image" />
+            <Card.ImgOverlay className="SignUp-image">
+              <Card.Title as={"h1"} className="SignUp-imageTitle">
+                {type}
+              </Card.Title>
+              <Card.Text>{message}</Card.Text>
+              <hr />
+            </Card.ImgOverlay>
+          </Card>
 
-        <Card>
-          <Form className="SignUp-Form" onSubmit={action}>
-            <Form.Group className="SignUp-FormGroup">
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                className="SignUp-EraseBorder "
-                type="text"
-                name="username"
-                placeholder="Examle123"
-                onChange={(e) => setUsername(e.target.value)}
-                value={username}
-              />
-            </Form.Group>
-            <Form.Group className="SignUp-FormGroup">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                className="SignUp-EraseBorder"
-                as={Form.Control}
-                name="password"
-                type="password"
-                placeholder="• • • • • • • •"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </Form.Group>
-            {errorMsg !== "" ? (
-              <p style={{ color: "#FF0000" }}>{errorMsg}</p>
-            ) : (
-              <></>
-            )}
-            <Button className="SignUp-Button" type="submit">
-              {type}
-            </Button>
-            <br />
-            <br />
-            <p className="SignUp-SignIn">
-              Already have an account?
-              {type === "Log In" ? (
-                <a href="signup">Sign Up</a>
+          <Card>
+            <Form className="SignUp-Form" onSubmit={action}>
+              <Form.Group className="SignUp-FormGroup">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  className="SignUp-EraseBorder "
+                  type="text"
+                  name="username"
+                  placeholder="Examle123"
+                  onChange={(e) => setUsername(e.target.value)}
+                  value={username}
+                />
+              </Form.Group>
+              <Form.Group className="SignUp-FormGroup">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className="SignUp-EraseBorder"
+                  as={Form.Control}
+                  name="password"
+                  type="password"
+                  placeholder="• • • • • • • •"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+              </Form.Group>
+              {errorMsg !== "" ? (
+                <p style={{ color: "#FF0000" }}>{errorMsg}</p>
               ) : (
-                <a href="login">Log In</a>
+                <></>
               )}
-            </p>
-          </Form>
-        </Card>
-      </CardGroup>
-      <br />
+              <Button className="SignUp-Button" type="submit">
+                {type}
+              </Button>
+              <br />
+              <br />
+              <p className="SignUp-SignIn">
+                Already have an account?
+                {type === "Log In" ? (
+                  <a href="signup">Sign Up</a>
+                ) : (
+                  <a href="login">Log In</a>
+                )}
+              </p>
+            </Form>
+          </Card>
+        </CardGroup>
+        <br />
+      </div>
     </div>
   );
 };
